c1b50514afed7acf510a40aeb48b1d39492edc97
Anas <anasmuhammed444@gmail.com>
2024-01-27 18:47:49 +0530

    assets  added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import '../globals.css';
 import { ClerkProvider } from "@clerk/nextjs";
 import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
@@ -26,11 +26,13 @@ export default function RootLayout({
           <Topbar />
           <main>
             <LeftSidebar />
+
             <section className="main-container">
               <div className="w-full max-w-4xl">
                 {children}
               </div>
             </section>
+            
             <RighSidebar />
             </main>
           <Bottombar />
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
 function Topbar() {
-    return <h1>Topbar</h1>
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flexitems-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Unite</p>
+      </Link>
+    </nav>
+  );
 }
-export default  Topbar
+export default Topbar;
